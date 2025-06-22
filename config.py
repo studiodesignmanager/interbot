0a34be5ef434bed16309c60bd36b9834d01129be
studiodesignmanager <studiodesignmanager@gmail.com>
2025-06-22 16:30:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = 'YOUR_BOT_TOKEN'
-CHANNEL = '@your_channel'
+TOKEN = '7841040731:AAEPoJTB0RhqiYKmBRC0e5LhttJ8s0XgzE0'
+CHANNEL = '@dating_service_channel'
 HEADERS = {'User-Agent': 'Mozilla/5.0'}
