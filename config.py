4bff0329a0e37f135bdff959448cedff5b454593
studiodesignmanager <studiodesignmanager@gmail.com>
2025-06-22 17:12:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ TOKEN = '7841040731:AAEPoJTB0RhqiYKmBRC0e5LhttJ8s0XgzE0'
 CHANNEL = '@dating_service_channel'
 HEADERS = {'User-Agent': 'Mozilla/5.0'}
 
-LOGIN_URL = "https://findbride.com/login"  # проверь, если другой, замени сюда
+LOGIN_URL = "https://findbride.com/login"
 LOGIN_DATA = {
     "email": "guestodessa1@gmail.com",
     "password": "8gr$DwXQz"
